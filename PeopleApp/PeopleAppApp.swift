0485429999cbb7cacf957d9b09b9fa2ba65b8962
Sander Haug <sander_haug@hotmail.com>
2023-06-13 14:51:14 +0200

    Initial Commit

---
+++ PeopleApp/PeopleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleAppApp.swift
+//  PeopleApp
+//
+//  Created by Sander Haug on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
